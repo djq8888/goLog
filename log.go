6f07dd45ee1478fe6f6c285a0afa74d2eea92aa0
DJQ <13211080@bjtu.edu.cn>
2020-03-09 23:49:14 +0800

    make goLog a package that canbe used in a project

--- log.go
+++ log.go
@@ -1,29 +1,50 @@
-package main
+package goLog
 
 import (
-	"fmt"
-	"io/ioutil"
 	"os"
 	"time"
 )
 
-func logInfo()  {
-	
-}
+var filename string
+var logString = make(chan string)
 
 func init()  {
 	//创建日志文件夹
 	os.Mkdir("./log",0777)
 	//创建日志文件
 	createTime := time.Now().Format("2006-01-02_15_04_05")
-	filename := "log/" + createTime + ".txt"
-	os.Create(filename)
+	filename = "log/" + createTime + ".txt"
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE, 0666)
+	if err == nil {
+		defer file.Close()
+	} else {
+		return
+	}
 	//写入初始日志
 	initInfo := "logfile is created at " + createTime
-	ioutil.WriteFile(filename, []byte(initInfo), 0666)
+	file.Write([]byte(initInfo))
+	//开启日志写入协程
+	go func() {
+		for {
+			select {
+			case buf := <- logString:
+				log(buf)
+			}
+		}
+	}()
 }
 
-func main()  {
-	//初始化
-	fmt.Println(1)
+func log(buf string)  {
+	tmp := "\n" + time.Now().Format("2006-01-02_15_04_05") + " " + buf
+	file, err := os.OpenFile(filename, os.O_APPEND, 0666)
+	if err == nil {
+		defer file.Close()
+	} else {
+		return
+	}
+	file.Write([]byte(tmp))
+}
+
+func Log(buf string)  {
+	logString <- buf
 }
